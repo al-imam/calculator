f367e896ffcd4d9079e13588f10ac531c824268a
al-imam <alimam01828@gmail.com>
2023-03-08 04:37:20 +0600

    modify button component to take span

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 const Button: React.FunctionComponent<{
   click: (value: string) => void;
   value: string;
-}> = ({ click, value }) => (
+  span?: string;
+}> = ({ click, value, span = "" }) => (
   <button
     onClick={() => click(value)}
-    className="rounded-sm text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900 active:bg-cyan-500"
+    className={
+      "rounded-sm text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900 active:bg-cyan-500 " +
+      span
+    }
   >
     {value}
   </button>
