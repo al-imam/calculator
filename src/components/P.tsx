e0e7d5157427ea53069620aa368e300bc235ea44
al-imam <alimam01828@gmail.com>
2023-03-08 05:23:28 +0600

    use new manrope font

--- src/components/P.tsx
+++ src/components/P.tsx
@@ -2,7 +2,9 @@ const P: React.FunctionComponent<{
   children: React.ReactNode;
   classes?: string;
 }> = ({ children, classes }) => (
-  <p className={`select-none antialiased break-words break-all ${classes}`}>
+  <p
+    className={`font-manrope select-none antialiased break-words break-all ${classes}`}
+  >
     {children}
   </p>
 );
