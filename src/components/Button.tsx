50d474a6c427b139f83724e635e709229e0d03c8
al-imam <alimam01828@gmail.com>
2023-03-07 15:58:15 +0600

    style button component active state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: React.FunctionComponent<{
 }> = ({ click, value }) => (
   <button
     onClick={() => click(value)}
-    className="rounded-sm text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900"
+    className="rounded-sm text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900 active:bg-cyan-500"
   >
     {value}
   </button>
