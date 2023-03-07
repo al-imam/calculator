af6d4b2ef746dbf01418e858511b2396068ad392
al-imam <alimam01828@gmail.com>
2023-03-07 15:01:22 +0600

    create full calculator markup

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,24 @@ function App() {
         <p>10000 + {/* demo text */}</p>
         <p>10000 {/* demo text */}</p>
       </section>
+      <Button click={() => {}} value="C" />
+      <Button click={() => {}} value="DE" />
+      <Button click={() => {}} value="/" />
+      <Button click={() => {}} value="1" />
+      <Button click={() => {}} value="2" />
+      <Button click={() => {}} value="3" />
+      <Button click={() => {}} value="*" />
+      <Button click={() => {}} value="4" />
+      <Button click={() => {}} value="5" />
+      <Button click={() => {}} value="6" />
+      <Button click={() => {}} value="-" />
+      <Button click={() => {}} value="7" />
+      <Button click={() => {}} value="8" />
+      <Button click={() => {}} value="9" />
+      <Button click={() => {}} value="+" />
+      <Button click={() => {}} value="." />
       <Button click={() => {}} value="0" />
+      <Button click={() => {}} value="=" />
     </main>
   );
 }
