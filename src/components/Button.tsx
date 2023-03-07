60d186f1b7901b708bf96ead79365ac4f0f6941f
al-imam <alimam01828@gmail.com>
2023-03-07 15:36:17 +0600

    style button component focus-within state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: React.FunctionComponent<{
 }> = ({ click, value }) => (
   <button
     onClick={() => click(value)}
-    className="rounded-sm text-3xl bg-slate-900 text-indigo-300 border-none outline-none shadow-md transition"
+    className="rounded-sm text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-within:outline-solid focus-within:outline-offset-0 focus-within:outline-indigo-500"
   >
     {value}
   </button>
