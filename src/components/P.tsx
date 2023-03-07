c6cef8ebd15dfc602f5949ca1167c0d2aaf87134
al-imam <alimam01828@gmail.com>
2023-03-08 05:10:54 +0600

    prevent selecting button text

--- src/components/P.tsx
+++ src/components/P.tsx
@@ -2,7 +2,9 @@ const P: React.FunctionComponent<{
   children: React.ReactNode;
   classes?: string;
 }> = ({ children, classes }) => (
-  <p className={`antialiased break-words break-all ${classes}`}>{children}</p>
+  <p className={`select-none antialiased break-words break-all ${classes}`}>
+    {children}
+  </p>
 );
 
 export default P;
