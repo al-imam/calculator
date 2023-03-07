8e2d1a1dea3bb7ed830c9cfe4bdb3367a8530d66
al-imam <alimam01828@gmail.com>
2023-03-08 04:50:04 +0600

    change css style logic use flex

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import P from "./components/P";
 function App() {
   return (
     <main className="mt-12 grid justify-center gap-2 grid-template">
-      <section className="col-span-full">
+      <section className="col-span-full bg-blue-400 flex flex-col justify-center items-end px-4 py-2">
         <P classes="text-lg">10000 + {/* demo text */}</P>
         <P classes="text-3xl">10000 {/* demo text */}</P>
       </section>
--- src/components/P.tsx
+++ src/components/P.tsx
@@ -2,9 +2,7 @@ const P: React.FunctionComponent<{
   children: React.ReactNode;
   classes?: string;
 }> = ({ children, classes }) => (
-  <p className={`clear-both float-right break-words break-all ${classes}`}>
-    {children}
-  </p>
+  <p className={`break-words break-all ${classes}`}>{children}</p>
 );
 
 export default P;
