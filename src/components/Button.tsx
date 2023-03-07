ee296bbb74bef2f01934296ad1659bccbf229f95
al-imam <alimam01828@gmail.com>
2023-03-07 15:28:50 +0600

    style button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,12 @@ const Button: React.FunctionComponent<{
   click: (value: string) => void;
   value: string;
 }> = ({ click, value }) => (
-  <button onClick={() => click(value)}>{value}</button>
+  <button
+    onClick={() => click(value)}
+    className="rounded-sm text-3xl bg-slate-900 text-indigo-50 border-none outline-none"
+  >
+    {value}
+  </button>
 );
 
 export default Button;
