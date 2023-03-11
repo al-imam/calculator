a730e9f1a92e89a09bcb946c6eaaf94a784e5bc0
al-imam <alimam01828@gmail.com>
2023-03-11 15:47:33 +0600

    sort tailwind classes in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,10 +5,7 @@ const Button: React.FunctionComponent<{
 }> = ({ click, value, span = "" }) => (
   <button
     onClick={() => click(value)}
-    className={
-      "font-manrope antialiased rounded select-none text-3xl bg-white text-blue-900 dark:bg-slate-900 dark:text-indigo-100 hover:bg-slate-100 dark:hover:bg-gray-900 active:bg-teal-200 dark:active:bg-blue-500 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 " +
-      span
-    }
+    className={`text-3xl antialiased text-blue-900 transition bg-white border-none rounded shadow-md outline-none select-none font-manrope dark:bg-slate-900 dark:text-indigo-100 hover:bg-slate-100 dark:hover:bg-gray-900 active:bg-teal-200 dark:active:bg-blue-500 focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 ${span}`}
   >
     {value}
   </button>
