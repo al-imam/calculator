d7fde1298dc76684bf3daae9d454b17078f59a45
al-imam <alimam01828@gmail.com>
2023-03-11 15:02:57 +0600

    change button bg and fg on light theme

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const Button: React.FunctionComponent<{
   <button
     onClick={() => click(value)}
     className={
-      "font-manrope antialiased rounded select-none text-3xl bg-slate-900 text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900 active:bg-blue-500 " +
+      "font-manrope antialiased rounded select-none text-3xl bg-white text-blue-900 dark:bg-slate-900 dark:text-indigo-100 border-none outline-none shadow-md transition focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 hover:bg-gray-900 active:bg-blue-500 " +
       span
     }
   >
