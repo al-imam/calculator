9b1a8283f64255dc2f3f5315fed1a3ba80d6e058
al-imam <alimam01828@gmail.com>
2023-03-16 20:03:06 +0600

    configure pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        description: "Don't go away from math it's nature! 🥹",
+        name: "calculator",
+        short_name: "calculator",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        categories: ["tools", "program"],
+        orientation: "portrait",
+        lang: "en",
+      },
+    }),
+  ],
+});
