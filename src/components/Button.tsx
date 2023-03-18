ca447511e8d45bdc228f86a857079138c383bed5
al-imam <alimam01828@gmail.com>
2023-03-19 04:44:15 +0600

    change focus style outline to shadow

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button: React.FunctionComponent<{
 }> = ({ click, value, span = "" }) => (
   <button
     onClick={() => click(value)}
-    className={`text-3xl antialiased text-blue-900 transition bg-white border-none rounded shadow-md outline-none select-none font-manrope dark:bg-slate-900 dark:text-indigo-100 hover:bg-slate-100 dark:hover:bg-gray-900 active:bg-teal-200 dark:active:bg-blue-500 focus-visible:outline-solid focus-visible:outline-offset-0 focus-visible:outline-indigo-500 ${span}`}
+    className={`text-3xl antialiased text-blue-900 transition bg-white border-none rounded shadow-md outline-none select-none font-manrope dark:bg-slate-900 dark:text-indigo-100 hover:bg-slate-100 dark:hover:bg-gray-900 active:bg-teal-200 dark:active:bg-blue-500 focus-visible:ring-2 ring-indigo-500 ${span}`}
   >
     {value}
   </button>
