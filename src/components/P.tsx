bae1f012eaaadca8bdb650afb1e44b1ecc1f8e56
al-imam <alimam01828@gmail.com>
2023-03-19 06:13:41 +0600

    calculate width and if it overflowing then do something

--- src/components/P.tsx
+++ src/components/P.tsx
@@ -1,12 +1,43 @@
+import { useEffect, useState, useRef } from "react";
+
 const P: React.FunctionComponent<{
   children: React.ReactNode;
   classes?: string;
-}> = ({ children, classes }) => (
-  <p
-    className={`font-manrope select-none antialiased break-words break-all ${classes}`}
-  >
-    {children}
-  </p>
-);
+  sectionRef: HTMLDivElement;
+  overflow?: string;
+  notOverflow?: string;
+}> = ({ children, classes, sectionRef, overflow, notOverflow }) => {
+  const ref = useRef<HTMLParagraphElement>(null);
+  const [isLarge, setLarge] = useState(false);
+
+  useEffect(() => {
+    if (ref.current && sectionRef) {
+      const myWith = ref.current.clientWidth + 16;
+      const parentWidth =
+        sectionRef.clientWidth -
+        parseFloat(
+          getComputedStyle(sectionRef).paddingInline.replace("px", "")
+        ) *
+          2;
+
+      if (myWith > parentWidth) {
+        setLarge(true);
+      } else {
+        setLarge(false);
+      }
+    }
+  });
+
+  return (
+    <p
+      ref={ref}
+      className={`font-manrope select-none antialiased break-words break-all ${classes} ${
+        isLarge ? overflow : notOverflow
+      }}`}
+    >
+      {children}
+    </p>
+  );
+};
 
 export default P;
