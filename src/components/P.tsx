b79e3b84a7ff08436ecb11d0dac29ca16b9108e3
al-imam <alimam01828@gmail.com>
2023-03-19 06:52:37 +0600

    roll back

--- src/components/P.tsx
+++ src/components/P.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useState, useRef } from "react";
-
 const P: React.FunctionComponent<{
   children: React.ReactNode;
   classes?: string;
-  sectionRef?: HTMLDivElement;
-  overflow?: string;
-  notOverflow?: string;
-}> = ({ children, classes, sectionRef, overflow, notOverflow }) => {
-  const ref = useRef<HTMLParagraphElement>(null);
-  const [isLarge, setLarge] = useState(false);
-
-  useEffect(() => {
-    if (ref.current && sectionRef) {
-      const myWith = ref.current.clientWidth + 16;
-      const parentWidth =
-        sectionRef.clientWidth -
-        parseFloat(
-          getComputedStyle(sectionRef).paddingInline.replace("px", "")
-        ) *
-          2;
-      console.log({ myWith, parentWidth, isLarge });
-      if (myWith > parentWidth) {
-        setLarge(true);
-        return () => setLarge(false);
-      }
-      setLarge(false);
-    }
-  });
-
-  return (
-    <p
-      ref={ref}
-      className={`font-manrope select-none antialiased break-words break-all ${classes} ${
-        isLarge ? overflow : notOverflow
-      }}`}
-    >
-      {children}
-    </p>
-  );
-};
+}> = ({ children, classes }) => (
+  <p
+    className={`font-manrope select-none antialiased break-words break-all ${classes}`}
+  >
+    {children}
+  </p>
+);
 
 export default P;
